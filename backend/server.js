ebe110d4bc693b862405ea221ada28e0b9143530
Ubuntu <ubuntu@ip-172-31-45-123.us-east-2.compute.internal>
2026-06-18 09:09:47 +0000

    Added CI/CD

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 
-const app = express();
+const app =  require("./app");
 
 app.use(cors());
 app.use(express.json());
@@ -10,8 +10,19 @@ app.use(express.json());
 mongoose.connect(
 "mongodb://mongodb:27017/employeedb"
 )
-.then(()=>console.log("MongoDB Connected"))
-.catch(err=>console.log(err));
+.then(()=>{
+
+console.log("Mongo Connected");
+
+app.listen(5000,()=>{
+console.log("Server Started");
+});
+
+})
+.catch(err=>{
+console.log(err);
+});
+
 
 const EmployeeSchema = new mongoose.Schema({
    name:String,
